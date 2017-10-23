e83345ab816ee224f7483dfc2e9189856941973d
Shufireyi <102338496@qq.com>
2017-10-23 22:36:22 +0800

    10.23 初始化目录结构 依赖

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+import tab from './components/tab/tab'
+export default {
+  components: {
+    MHeader: header,
+    tab
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
