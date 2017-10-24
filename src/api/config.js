7143ae275b597227f255746e24f4533e687d7a93
Shufireyi <102338496@qq.com>
2017-10-24 10:19:58 +0800

    10.24 修改eslint配置

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,8 @@ export const commonParams = {
   g_tk: 5381,
   inCharset: 'utf-8',
   outCharset: 'utf-8',
-  notice: 0
-  format: 'jsonp',
+  notice: 0,
+  format: 'jsonp'
 }
 
 export const options = {
